af8167e9016dfbdefc372eca1bfd625dca1351e4
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-09-16 12:31:20 -0300

    feat: cria navigation bottom bar

---
+++ lib/components/navigation_bar.dart
@@ -0,0 +1,59 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class FZBNavigationBar extends StatefulWidget {
+  const FZBNavigationBar({Key? key}) : super(key: key);
+
+  @override
+  State<FZBNavigationBar> createState() => _FZBNavigationBarState();
+}
+
+class _FZBNavigationBarState extends State<FZBNavigationBar> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.only(
+              topRight: Radius.circular(30), topLeft: Radius.circular(30)),
+          boxShadow: const [
+            BoxShadow(color: Colors.black38, spreadRadius: 0, blurRadius: 10),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(30.0),
+            topRight: Radius.circular(30.0),
+          ),
+          child: BottomNavigationBar(
+              backgroundColor: Color.fromRGBO(25, 118, 210, 1),
+              fixedColor: Colors.white,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              items: const <BottomNavigationBarItem>[
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.search,
+                    ),
+                    label: "Procurar"),
+                BottomNavigationBarItem(
+                    icon: Icon(
+                      Icons.map_outlined,
+                    ),
+                    label: "Mapa"),
+                BottomNavigationBarItem(
+                    icon: Icon(Icons.person), label: "Usuário")
+              ],
+              currentIndex: _selectedIndex,
+              onTap: _onItemTapped),
+        ));
+  }
+}
