e83f13ddf005b483c22d30da25eb0bbea4a0ee99
C-Augus <ciroeaa@outlook.com>
2022-09-19 18:31:28 -0300

    refactor: drawer de configurações *

--- lib/widgets/drawer/appbar_drawer.dart
+++ lib/widgets/drawer/appbar_drawer.dart
@@ -11,45 +11,65 @@ class AppBarDrawer extends StatefulWidget {
 }
 
 class _AppBarDrawerState extends State<AppBarDrawer> {
+  final List<String> entries = <String>['Notificações', 'GPS', 'Sobre', 'Sair'];
+  final List<Color> textColor = <Color>[
+    Colors.black,
+    Colors.black,
+    Colors.black,
+    Colors.red
+  ];
+  final List<IconData> leadingIcon = <IconData>[
+    Icons.chat_bubble_outlined,
+    Icons.gps_fixed_outlined,
+    Icons.person_rounded,
+    Icons.exit_to_app_outlined
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      child: ListView(
-        children: [
-          DrawerHeader(
-            decoration: BoxDecoration(
-              color: Colors.white,
-            ),
-            child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: <Widget>[
-                  Text('Configurações',
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold))
-                ]),
+      child: Row(children: [
+        ListView.separated(
+          itemCount: entries.length,
+          itemBuilder: (context, index) {
+            return Container(
+                child: ListTile(
+              leading: Icon(leadingIcon[index], color: textColor[index]),
+              title: Text(entries[index]),
+            ));
+          },
+          separatorBuilder: (BuildContext context, int index) =>
+              const Divider(),
+        ),
+        DrawerHeader(
+          decoration: BoxDecoration(
+            color: Colors.white,
           ),
-          ListTile(
-            leading: Icon(Icons.chat_bubble_outlined),
-            title: const Text('Notificações'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.gps_fixed_outlined),
-            title: const Text('GPS'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.person_rounded),
-            title: const Text('Sobre'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.exit_to_app_outlined, color: Colors.red),
-            title: const Text('Sair'),
-            onTap: () {},
-          )
-        ],
-      ),
+          child: Text('Configurações'),
+        )
+      ]),
     );
   }
 }
+
+
+/*ListTile(
+          leading: Icon(Icons.chat_bubble_outlined),
+          title: const Text('Notificações'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.gps_fixed_outlined),
+          title: const Text('GPS'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.person_rounded),
+          title: const Text('Sobre'),
+          onTap: () {},
+        ),
+        ListTile(
+          leading: Icon(Icons.exit_to_app_outlined, color: Colors.red),
+          title: const Text('Sair'),
+          onTap: () {},
+        )*/
