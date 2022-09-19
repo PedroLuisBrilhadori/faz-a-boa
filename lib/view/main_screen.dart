b20c0a49725faaf8743d95af76459431946454dd
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-09-19 17:40:44 -0300

    feat: cria um widget para os cards dos postos

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:faz_a_boa/widgets/app_bar/app_bar.dart';
+import 'package:faz_a_boa/widgets/card/card.dart';
 import 'package:faz_a_boa/widgets/navigation_bar/navigation_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +15,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: FZBAppBar("Postos"),
+      body: Center(child: CardWidget()),
       bottomNavigationBar: FZBNavigationBar(),
     );
   }
