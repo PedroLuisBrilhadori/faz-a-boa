e55c01fcaadcb7a5313b0f1f9da61ce0e3a724bf
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-09-19 20:30:30 -0300

    fix: arruma alinhamento das informações do card

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:faz_a_boa/widgets/app_bar/app_bar.dart';
 import 'package:faz_a_boa/widgets/card/card.dart';
 import 'package:faz_a_boa/widgets/navigation_bar/navigation_bar.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,19 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: CardWidget()),
+      body: Column(children: [
+        FZBAppBar('Postos'),
+        Expanded(
+            child: ListView(
+          children: const [
+            CardWidget(),
+            CardWidget(),
+            CardWidget(),
+            CardWidget(),
+            CardWidget(),
+          ],
+        )),
+      ]),
       bottomNavigationBar: FZBNavigationBar(),
       endDrawer: ConfigDrawer(),
     );
