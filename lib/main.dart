f3057ee4a8b1added8a810994f7a4df0626f4bec
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-09-19 22:07:42 -0300

    feat: esconde a status bar do celular

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 import 'package:faz_a_boa/view/main_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
+
+  SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) =>
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack));
+
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'faz A boa',
