a4ba60ce0536391730ffb0a71ac0d47717ceecd8
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-09-21 10:47:38 -0300

    feat: esconde a barra de navegação do celular

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:flutter/services.dart';
 
 import 'app/app.module.dart';
 import 'app/app.widget.dart';
 
-void main() =>
-    runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.top]);
+
+  SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) =>
+      SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+          overlays: [SystemUiOverlay.top]));
+
+  runApp(ModularApp(module: AppModule(), child: const AppWidget()));
+}
