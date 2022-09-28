be0cdd9722d9f3f158d80f49c48d6bc737e400a6
C-Augus <ciroeaa@outlook.com>
2022-09-28 12:15:37 -0300

    feat: criação da tela de login

--- lib/app/modules/login/login.page.dart
+++ lib/app/modules/login/login.page.dart
@@ -16,67 +16,80 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
           children: [
             //First Column()
-              Column(
+            Column(
               mainAxisAlignment: MainAxisAlignment.end,
               children: const [
-                  SizedBox(height: 120.0),
-                  Text('Entrar',
+                SizedBox(height: 120.0),
+                Text('Entrar',
                     style: TextStyle(fontSize: 27, fontWeight: FontWeight.w700))
-              ],),
+              ],
+            ),
             //Second Column()
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 60.0),
-                  textField('Email', login),
-                  const SizedBox(height: 5.0),
-                  textField('Senha', password),
-                  loginButton('Confirmar', context),
-                ],
-              ),
-            //Third Column()
-                Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 60.0),
-                  Text(
-                    'Ou continue com:',
-                    style: TextStyle(color: Colors.grey[700], fontSize: 16),
-                  ),                  const SizedBox(
-                    height: 7.0,
-                  ),
-                  connectWithButton(Buttons.FacebookNew, 'Facebook'),
-                  connectWithButton(Buttons.Google, 'Google'),
-                  const SizedBox(height: 15.0),
-                  RichText(
-                    text: TextSpan(
-                      style: TextStyle(
-                        fontSize: 17,
-                        color: Colors.grey[700],
-                      ),
-                      children: const <TextSpan> [
-                        TextSpan(text: 'Não tem uma conta?'),
-                        TextSpan(
-                          text: ' Crie agora!',
-                          style: TextStyle(color: Colors.black),
-                        ),
-                      ],
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60.0),
+                textField('Email', login),
+                const SizedBox(height: 5.0),
+                textField('Senha', password),
+                const SizedBox(height: 10.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const Text(
+                      'Esqueceu sua senha?',
+                      style: TextStyle(color: Colors.grey, fontSize: 16),
                     ),
+                    SizedBox(
+                      width: screenWidth * 0.077,
+                    )
+                  ],
+                ),
+                loginButton('Confirmar', context),
+              ],
+            ),
+            //Third Column()
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const SizedBox(height: 60.0),
+                Text(
+                  'Ou continue com:',
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16),
+                ),
+                const SizedBox(
+                  height: 7.0,
+                ),
+                connectWithButton(Buttons.FacebookNew, 'Facebook'),
+                connectWithButton(Buttons.Google, 'Google'),
+                const SizedBox(height: 15.0),
+                RichText(
+                  text: TextSpan(
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: Colors.grey[700],
+                    ),
+                    children: const <TextSpan>[
+                      TextSpan(text: 'Não tem uma conta?'),
+                      TextSpan(
+                        text: ' Crie agora!',
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 7.0),
-                  const Text(
-                    'Esqueceu sua senha?',
-                    style: TextStyle(color: Colors.black, fontSize: 17),
-                  ),
-                ],
-              ),
+                ),
+                const SizedBox(height: 7.0),
+              ],
+            ),
           ],
         ),
       ),
