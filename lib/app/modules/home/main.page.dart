3d97502c0d43625cbf5f7c5f9c076f1f8e972425
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-10-18 12:53:37 -0300

    feat: cria scaffold base para navigationbar e config drawer

--- lib/app/modules/home/main.page.dart
+++ lib/app/modules/home/main.page.dart
@@ -1,13 +1,12 @@
 import 'package:faz_a_boa/app/models/station.model.dart';
 import 'package:faz_a_boa/app/services/stations.service.dart';
+import 'package:faz_a_boa/app/widgets/scaffold_base/scaffold_base.dart';
 import 'package:flutter/material.dart';
 
 import 'package:faz_a_boa/app/widgets/app_bar/app_bar.dart';
 import 'package:faz_a_boa/app/widgets/card/card.dart';
 import 'package:faz_a_boa/app/widgets/chips/list_chips.dart';
 import 'package:faz_a_boa/app/widgets/chips/model/chips.model.dart';
-import 'package:faz_a_boa/app/widgets/drawer/config_drawer.dart';
-import 'package:faz_a_boa/app/widgets/navigation_bar/navigation_bar.dart';
 
 List<ChipModel> chipsList = [
   ChipModel(label: 'Ordenar'),
@@ -28,63 +27,52 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.white,
-      body: SafeArea(child: AppColumn()),
-      bottomNavigationBar: FZBNavigationBar(),
-      endDrawer: ConfigDrawer(),
-    );
-  }
-}
-
-class AppColumn extends StatelessWidget {
-  const AppColumn({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 1,
-          child: FZBAppBar(
-            title: 'Postos',
-            child: ChipsListWidget(chipsList: chipsList),
+    return ScaffoldBase(
+      navigationBar: true,
+      configDrawer: true,
+      child: Column(
+        children: [
+          const Flexible(
+            flex: 1,
+            child: FZBAppBar(
+              title: 'Postos',
+            ),
           ),
-        ),
-        Flexible(
-          flex: 5,
-          child: FutureBuilder<List<Station>>(
-            future: StationService().getItems(),
-            builder: (context, snapshot) {
-              if (snapshot.hasError) {
-                return const Center(
-                  child: Text('Não foi possível exibir os dados'),
-                );
-              }
-
-              if (!snapshot.hasData) {
-                return const Center(child: CircularProgressIndicator());
-              }
-
-              final stations = snapshot.data;
-
-              return ListView.builder(
-                itemCount: stations!.length,
-                itemBuilder: (context, index) {
-                  return CardWidget(
-                    id: stations[index].id,
-                    title: stations[index].name,
-                    address: stations[index].address,
-                    distance: stations[index].distance,
-                    image: stations[index].image,
-                    rate: stations[index].rate,
+          Flexible(
+            flex: 5,
+            child: FutureBuilder<List<Station>>(
+              future: StationService().getItems(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text('Não foi possível exibir os dados'),
                   );
-                },
-              );
-            },
+                }
+
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
+
+                final stations = snapshot.data;
+
+                return ListView.builder(
+                  itemCount: stations!.length,
+                  itemBuilder: (context, index) {
+                    return CardWidget(
+                      id: stations[index].id,
+                      title: stations[index].name,
+                      address: stations[index].address,
+                      distance: stations[index].distance,
+                      image: stations[index].image,
+                      rate: stations[index].rate,
+                    );
+                  },
+                );
+              },
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
