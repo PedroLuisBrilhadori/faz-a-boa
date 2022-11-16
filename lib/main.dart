dd771744fbb137df97fde0918cbf726b4cc6cac5
PedroLuisBrilhadori <pedroluisbrilhadori@gmail.com>
2022-11-16 20:55:11 -0300

    feat: adiciona firebase no projeto

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:faz_a_boa/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import 'app/app.module.dart';
 import 'app/app.widget.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
       overlays: [SystemUiOverlay.top]);
@@ -13,6 +16,10 @@ void main() {
   SystemChrome.setSystemUIChangeCallback((systemOverlaysAreVisible) =>
       SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
           overlays: [SystemUiOverlay.top]));
+
+  await dotenv.load(fileName: ".env");
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(ModularApp(module: AppModule(), child: const AppWidget()));
 }
